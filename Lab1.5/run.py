ed5d5be80f10506560d5fa0d47937f8b8c900fc6
demidovanton1986 <demidovanton1986@gmail.com>
2024-03-15 17:43:18 +0300

    add Lab1.5

---
+++ Lab1.5/run.py
@@ -0,0 +1,12 @@
+import glob
+import os
+
+for file in glob.iglob(os.path.join(os.getcwd(), "config_files", "*.log")):
+    with open(file, encoding="utf-8") as f:
+        result = set()
+        for line in f:
+            line = line.split("ip address")[-1].strip() if "ip address" in line else ""
+            if line:
+                result.add(line)
+        print(f"В файле {file} содержатся следующие адреса:")
+        print(*result, sep="\n", end="\n\n")
