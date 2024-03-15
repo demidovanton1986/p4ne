c632a30b40c68ee918ae1dec3eb57972a8eed884
demidovanton1986 <demidovanton1986@gmail.com>
2024-03-15 17:48:23 +0300

    change run.py in Lab1.5 (add rstrip("sub"))

--- Lab1.5/run.py
+++ Lab1.5/run.py
@@ -5,7 +5,7 @@ for file in glob.iglob(os.path.join(os.getcwd(), "config_files", "*.log")):
     with open(file, encoding="utf-8") as f:
         result = set()
         for line in f:
-            line = line.split("ip address")[-1].strip() if "ip address" in line else ""
+            line = line.split("ip address")[-1].strip().rstrip("sub").rstrip() if "ip address" in line else ""
             if line:
                 result.add(line)
         print(f"В файле {file} содержатся следующие адреса:")
